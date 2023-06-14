7428922e6af01e5865554afb2f8dee887410ce3d
Keyur Raval <ravalkeyur054@gmail.com>
2023-06-14 20:03:12 +0530

    changes

--- components/UserHeader.js
+++ components/UserHeader.js
@@ -54,7 +54,7 @@ const UserHeader = () => {
                         </button>
                     </Link>
                 </div>
-                <Link href={`http://localhost:3000/${handle}`}>
+                <Link href={`https://linktree-ft.vercel.app/${handle}`}>
                     <div className="flex flex-row">
                         <div className='inline-flex mr-5 text-right items-center bg-gray-200 px-5 py-2 rounded-lg'>
                             <div className="text-xs md:text-md flex flex-col flex-wrap">
